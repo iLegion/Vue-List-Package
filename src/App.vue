a5499cf00517c42da7cb12eba66033fbbbae447e
Yusif Zourab <wordlearningpress@gmail.com>
2019-10-31 17:32:12 +0200

    Added new component.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <template v-if="countItems">
+            <ul class="list-group">
+                <item v-for="(dataItem, index) in dataItems"
+                      :key="index"
+                      :data="dataItem"
+                ></item>
+            </ul>
+        </template>
+        <template v-else>{{ emptyListMsg }}</template>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Item from './components/Item.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Item,
+        },
+        props: {
+            listType: String,
+        },
+        computed: {
+            type: function () {
+                let type = this.listType ? this.listType : 'ul';
+
+                if (this.listTypes.includes(this.listType)) {
+                    return type;
+                }
+                // return ;
+            },
+            countItems: function () {
+                return this.dataItems.length;
+            },
+        },
+        data() {
+            return {
+                listTypes: ['ul', 'ol', 'div'],
+                dataItems: ['One'],
+                emptyListMsg: 'List is empty.',
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    #app {
+        .list-group {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+
+            .list-group-item {
+                &:first-child {
+                    border-top-left-radius: .25rem;
+                    border-top-right-radius: .25rem;
+                }
+
+                position: relative;
+                display: block;
+                padding: .75rem 1.25rem;
+                margin-bottom: -1px;
+                background-color: #fff;
+                border: 1px solid rgba(0, 0, 0, .125);
+            }
+        }
+    }
 </style>
---
+++ src/components/Item.vue
@@ -0,0 +1,16 @@
+<template>
+    <li class="list-group-item">{{ data }}</li>
+</template>
+
+<script>
+    export default {
+        name: "Item",
+        props: {
+            data: String,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
